f7bfeb48caea0f3c7d42eab1720d7de6c1ee203e
Akshat Sipany <118999195+akshat-code21@users.noreply.github.com>
2025-02-01 17:49:54 +0530

    added dummy route

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ import insightsRouter from "./routes/insights"
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/',(req,res)=>{
+  res.json({
+    message : "PennyWise Backend"
+  })
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/expenses", expenseRouter);
 app.use("/api/v1/insights", insightsRouter);
